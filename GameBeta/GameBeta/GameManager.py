ee4f5fa0da8170b4c11331024503519906eec191
IElecec <2231573241@qq.com>
2024-01-05 10:10:44 +0800

    Add_guide

---
+++ GameBeta/GameBeta/GameManager.py
@@ -0,0 +1,257 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import pygame
+
+from Player import Player
+from Scene import *
+from Settings import *
+from PopUpBox import *
+from Guide import *
+
+class GameManager:
+    def __init__(self):
+        
+        ##### Your Code Here ↓ #####
+        #初始游戏状态
+        self.state=GameState.MAIN_MENU
+        #设置窗口和时钟
+        self.window=pygame.display.set_mode((WindowSettings.width,WindowSettings.height))
+        pygame.display.set_caption(WindowSettings.name)
+        self.clock=pygame.time.Clock()
+        #设置碰撞检测器
+        self.collide=Collidable()
+
+        self.scene=StartMenu(self.window) 
+
+        self.player=Player(WindowSettings.width//2,WindowSettings.height//2)  
+
+        self.guideboard=Guideboard(self.window)
+
+        ##### Your Code Here ↑ #####
+
+    def game_reset(self):
+
+        ##### Your Code Here ↓ #####
+        pass
+        ##### Your Code Here ↑ #####
+
+    # Necessary game components here ↓
+    def tick(self, fps):
+        ##### Your Code Here ↓ #####
+        self.clock.tick(fps)
+        ##### Your Code Here ↑ #####
+
+    def get_time(self):
+        ##### Your Code Here ↓ #####
+        return pygame.time.get_ticks()
+        ##### Your Code Here ↑ #####
+
+    # Scene-related update functions here ↓
+    def flush_scene(self, GOTO:SceneType):
+        ##### Your Code Here ↓ #####
+        if GOTO==SceneType.CITY:
+            self.scene=CityScene(self.window)
+            self.player.reset_pos()
+        if GOTO==SceneType.WILD:
+            self.scene=WildScene(self.window)
+            self.player.reset_pos()
+        if GOTO==SceneType.BOSS:
+            self.scene=BossScene(self.window)
+            self.player.reset_pos()
+        self.player.player_attack_wave.empty()
+        ##### Your Code Here ↑ #####
+
+    def update(self):
+        ##### Your Code Here ↓ #####
+        self.tick(30)
+        if self.state==GameState.MAIN_MENU:
+            self.update_main_menu(pygame.event.get())
+        else:
+            if self.state==GameState.GAME_PLAY_CITY:
+                self.update_city(pygame.event.get())
+            if self.state==GameState.GAME_PLAY_WILD:
+                self.update_wild(pygame.event.get())
+            if self.state==GameState.GAME_PLAY_BOSS:
+                self.update_boss(pygame.event.get())
+            self.player.attack()
+            self.update_attack()
+            self.guideboard.update()
+        ##### Your Code Here ↑ #####
+
+    def update_main_menu(self, events):
+        ##### Your Code Here ↓ #####
+        for event in events:
+            if event.type==pygame.QUIT:
+                pygame.QUIT()
+                sys.exit()
+        keys=pygame.key.get_pressed()
+        if any(keys):
+            self.state=GameState.GAME_PLAY_CITY
+            self.flush_scene(SceneType.CITY)
+        ##### Your Code Here ↑ #####
+
+    def update_city(self, events):
+        # Deal with EventQueue First
+        ##### Your Code Here ↓ #####
+        for event in events:
+            if event.type==pygame.QUIT:
+                pygame.QUIT()
+                sys.exit()
+        
+        ##### Your Code Here ↑ #####
+
+        # Then deal with regular updates
+        ##### Your Code Here ↓ #####
+        self.player.try_move()
+        self.update_collide()
+        if self.collide.is_colliding():
+            if self.collide.collidingWith["portal"]:
+                if self.collide.collidingObject["portal"].GOTO==SceneType.WILD:
+                    self.state=GameState.GAME_PLAY_WILD
+                    self.flush_scene(SceneType.WILD)
+                    self.collide.collidingObject["portal"].kill()
+            if self.collide.collidingWith["obstacle"]:
+                self.player.rect=self.player.rect.move(-self.player.dx,-self.player.dy)
+        self.scene.update_camera(self.player)
+        ##### Your Code Here ↑ #####
+
+    def update_wild(self, events):
+        # Deal with EventQueue First
+        ##### Your Code Here ↓ #####
+        for event in events:
+            if event.type==pygame.QUIT:
+                pygame.QUIT()
+                sys.exit()
+        ##### Your Code Here ↑ #####
+        
+        # Then deal with regular updates
+        ##### Your Code Here ↓ #####
+        self.player.try_move()
+        self.update_collide()
+        if self.collide.is_colliding():
+            if self.collide.collidingWith["portal"]:
+                if self.collide.collidingObject["portal"].GOTO==SceneType.CITY:
+                    self.state=GameState.GAME_PLAY_CITY
+                    self.flush_scene(SceneType.CITY)
+                    self.collide.collidingObject["portal"].kill()
+                if self.collide.collidingObject["portal"].GOTO==SceneType.BOSS:
+                    self.state=GameState.GAME_PLAY_BOSS
+                    self.flush_scene(SceneType.BOSS)
+                    self.collide.collidingObject["portal"].kill()
+            if self.collide.collidingWith["obstacle"]:
+                self.player.rect=self.player.rect.move(-self.player.dx,-self.player.dy)
+        self.scene.update_camera(self.player)
+        ##### Your Code Here ↑ #####
+
+    def update_boss(self, events):
+        # Deal with EventQueue First
+        ##### Your Code Here ↓ #####
+        for event in events:
+            if event.type==pygame.QUIT:
+                pygame.QUIT()
+                sys.exit()
+        ##### Your Code Here ↑ #####
+        
+        # Then deal with regular updates
+        ##### Your Code Here ↓ #####
+        self.player.try_move()
+        self.update_collide()
+        if self.collide.is_colliding():
+            if self.collide.collidingWith["portal"]:
+                if self.collide.collidingObject["portal"].GOTO==SceneType.WILD:
+                    self.state=GameState.GAME_PLAY_WILD
+                    self.flush_scene(SceneType.WILD)
+                    self.collide.collidingObject["portal"].kill()
+            if self.collide.collidingWith["obstacle"]:
+                self.player.rect=self.player.rect.move(-self.player.dx,-self.player.dy)
+        self.scene.update_camera(self.player)
+        ##### Your Code Here ↑ #####
+    def update_attack(self):
+        for attack in self.player.player_attack_wave:
+            attack.update()
+            if attack.over_range(self.scene.cameraX,self.scene.cameraY):
+                attack.kill()
+    # Collision-relate update funtions here ↓
+    def update_collide(self):
+        # Player -> Obstacles
+        ##### Your Code Here ↓ #####
+        if pygame.sprite.spritecollide(self.player,self.scene.obstacles,False,pygame.sprite.collide_mask):
+            self.collide.collidingWith["obstacle"]=True
+            for obstacle in self.scene.obstacles.sprites():
+                if pygame.sprite.collide_rect(self.player,obstacle):
+                    self.collide.collidingObject["obstacle"].append(obstacle)
+        else:
+            self.collide.collidingWith["obstacle"]=False
+            self.collide.collidingObject["obstacle"]=[]
+        ##### Your Code Here ↑ #####
+
+        # Player -> NPCs; if multiple NPCs collided, only first is accepted and dealt with.
+        ##### Your Code Here ↓ #####
+        pass
+        ##### Your Code Here ↑ #####
+
+        # Player -> Monsters
+        ##### Your Code Here ↓ #####
+        pass
+        ##### Your Code Here ↑ #####
+        
+        # Player -> Portals
+        ##### Your Code Here ↓ #####
+        if pygame.sprite.spritecollide(self.player,self.scene.portals,False,pygame.sprite.collide_mask):
+            self.collide.collidingWith["portal"]=True
+            for portal in self.scene.portals.sprites():
+                if pygame.sprite.collide_rect(self.player,portal):
+                    self.collide.collidingObject["portal"]=portal
+        else:
+            self.collide.collidingWith["portal"]=False
+            self.collide.collidingObject["portal"]=None
+        ##### Your Code Here ↑ #####
+        
+        # Player -> Boss
+        ##### Your Code Here ↓ #####
+        pass
+        ##### Your Code Here ↑ #####
+
+    def update_NPCs(self):
+        # This is not necessary. If you want to re-use your code you can realize this.
+        ##### Your Code Here ↓ #####
+        pass
+        ##### Your Code Here ↑ #####
+
+    # Render-relate update functions here ↓
+    def render(self):
+        ##### Your Code Here ↓ #####
+        if self.state==GameState.MAIN_MENU:
+            self.render_main_menu()
+        else:
+            if self.state==GameState.GAME_PLAY_CITY:
+                self.render_city()
+            if self.state==GameState.GAME_PLAY_WILD:
+                self.render_wild()
+            if self.state==GameState.GAME_PLAY_BOSS:
+                self.render_boss()
+            self.guideboard.draw()
+        ##### Your Code Here ↑ #####
+    
+    def render_main_menu(self):
+        ##### Your Code Here ↓ #####
+        self.scene.render(self.get_time())
+        ##### Your Code Here ↑ #####
+    
+    def render_city(self):
+        ##### Your Code Here ↓ #####
+        self.scene.render(self.player)
+        ##### Your Code Here ↑ #####
+
+    def render_wild(self):
+        ##### Your Code Here ↓ #####
+        self.scene.render(self.player)
+        ##### Your Code Here ↑ #####
+
+    def render_boss(self):
+        ##### Your Code Here ↓ #####
+        self.scene.render(self.player)
+        ##### Your Code Here ↑ #####
+
+
---
+++ GameBeta/GameBeta/Player.py
@@ -0,0 +1,153 @@
+# -*- coding:utf-8 -*-
+
+import pygame
+
+from Settings import *
+from Attributes import *
+
+class Player(pygame.sprite.Sprite, Collidable):
+    def __init__(self, x, y):
+        # Must initialize everything one by one here
+        pygame.sprite.Sprite.__init__(self)
+        Collidable.__init__(self)
+
+        ##### Your Code Here ↓ #####
+        self.images=[pygame.transform.scale(pygame.image.load(img),(PlayerSettings.playerWidth,PlayerSettings.playerHeight)) for img in GamePath.player]
+        self.index=0
+        self.image=self.images[self.index]
+        self.dir=1
+        self.rect=self.image.get_rect()
+        self.rect.topleft=(x,y)
+        self.speed=PlayerSettings.playerSpeed
+        self.dx=0
+        self.dy=0
+        self.attack_cooldown=PlayerSettings.playerAttackCooldown
+        self.attack_speed = PlayerSettings.playerAttackSpeed
+        self.player_attack_wave=pygame.sprite.Group()#self.player_attack_wave = []
+        self.player_attack_range = PlayerSettings.playerAttackRange
+        self.player_last_attack_time = 0
+        
+        ##### Your Code Here ↑ #####
+    def attr_update(self, addCoins = 0, addHP = 0, addAttack = 0, addDefence = 0):
+        ##### Your Code Here ↓ #####
+        pass
+        ##### Your Code Here ↑ #####
+
+    def reset_pos(self, x=WindowSettings.width // 2, y=WindowSettings.height // 2):
+        ##### Your Code Here ↓ #####
+        self.rect.topleft=(x,y)
+        ##### Your Code Here ↑ #####
+
+    def try_move(self):
+        ##### Your Code Here ↓ #####
+        keys=pygame.key.get_pressed()
+        self.dx=0
+        self.dy=0
+        if keys[pygame.K_w] and self.rect.top > 0 :
+            self.dy -= self.speed
+        if keys[pygame.K_s] and self.rect.bottom< WindowSettings.height:
+            self.dy += self.speed
+        if keys[pygame.K_a] and self.rect.left> 0:
+            self.dx -= self.speed
+            if self.dir==1:
+                self.images=[pygame.transform.flip(img,True,False) for img in self.images]
+                self.dir=-1
+        if keys[pygame.K_d] and self.rect.right< WindowSettings.width:
+            self.dx += self.speed
+            if self.dir==-1:
+                self.images=[pygame.transform.flip(img,True,False) for img in self.images]
+                self.dir=1
+        self.rect=self.rect.move(self.dx,self.dy)
+        if self.dx!=0 or self.dy!=0:
+            self.index=(self.index+1)%len(self.images)
+            self.image=self.images[self.index]
+        ##### Your Code Here ↑ #####
+
+    def update(self,width,height):
+        ##### Your Code Here ↓ #####
+        pass
+        ##### Your Code Here ↑ #####
+    def draw(self, window,dx=0,dy=0):
+        ##### Your Code Here ↓ #####
+        self.rect=self.rect.move(dx,dy)
+        window.blit(self.image,self.rect)
+        ##### Your Code Here ↑ #####
+    def change_attack_speed(self):
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_o]:
+            self.attack_speed+=5
+        if keys[pygame.K_p] and self.attack_speed > 5 :
+            self.attack_speed-=5
+    def attack(self):
+        self.change_attack_speed()
+        # 玩家攻击
+        player_pos = [self.rect.x,self.rect.y]
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_j]:
+            current_time = pygame.time.get_ticks() / 1000
+            if current_time - self.player_last_attack_time > self.attack_cooldown:
+                self.player_attack_wave.add(Attack(player_pos[0], player_pos[1]+self.rect.height / 2,0,self.attack_speed))
+                self.player_last_attack_time = current_time
+
+        if keys[pygame.K_k]:
+            current_time = pygame.time.get_ticks() / 1000
+            if current_time - self.player_last_attack_time > self.attack_cooldown:
+                self.player_attack_wave.add(Attack(player_pos[0]+self.rect.width , player_pos[1]+self.rect.height / 2,1,self.attack_speed))
+                self.player_last_attack_time = current_time
+
+        if keys[pygame.K_i]:
+            current_time = pygame.time.get_ticks() / 1000.0
+            if current_time - self.player_last_attack_time > self.attack_cooldown:
+                self.player_attack_wave.add(Attack(player_pos[0]+self.rect.width / 2, player_pos[1],2,self.attack_speed))
+                self.player_last_attack_time = current_time
+
+        if keys[pygame.K_m]:
+            current_time = pygame.time.get_ticks() / 1000.0
+            if current_time - self.player_last_attack_time > self.attack_cooldown:
+                self.player_attack_wave.add(Attack(player_pos[0]+self.rect.width / 2, player_pos[1]+self.rect.height,3,self.attack_speed))
+                self.player_last_attack_time = current_time
+
+
+
+
+
+        #     # 检查是否击中BOSS
+        #     if (
+        #         boss_pos[0] < attack[0] < boss_pos[0] + boss_size
+        #         and boss_pos[1] < attack[1] < boss_pos[1] + boss_size
+        #     ):
+        #         boss_health -= player_attack
+        #         player_attack_wave.remove(attack)
+class Attack(pygame.sprite.Sprite):
+    def __init__(self,x,y,index,speed):
+        super().__init__()
+        ##### Your Code Here ↓ #####
+        self.images=[pygame.transform.scale(pygame.image.load(img),(PlayerSettings.playerAttackRange,PlayerSettings.playerAttackRange)) for img in GamePath.attack]
+        self.index=index
+        self.image=self.images[index]
+        self.rect=self.image.get_rect()
+        self.rect.topleft=(x,y)
+        self.attack_speed=speed
+        ##### Your Code Here ↑ #####
+    def update(self):
+        dx=dy=0
+        if self.index==0:
+            dx -= self.attack_speed
+        if self.index==1:
+            dx += self.attack_speed
+        if self.index==2:
+            dy -= self.attack_speed
+        if self.index==3: 
+            dy += self.attack_speed
+        self.rect=self.rect.move(dx,dy)
+    def over_range(self,cameraX,cameraY):
+        real_X=self.rect.x+cameraX
+        real_Y=self.rect.y+cameraY
+        if real_X<0 or real_Y<0 or real_X>WindowSettings.width * WindowSettings.outdoorScale or real_Y>WindowSettings.height * WindowSettings.outdoorScale:
+            return True
+        return False
+    def draw(self, window, dx=0, dy=0):
+        ##### Your Code Here ↓ #####
+        self.rect=self.rect.move(dx,dy)
+        window.blit(self.image,self.rect)
+        ##### Your Code Here ↑ #####
